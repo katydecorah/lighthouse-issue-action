ef7db8e5da1b44a195cba9febac70a1d7fa8ca47
Katy DeCorah <katydecorah@gmail.com>
2019-10-07 19:26:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const file = core.getInput('CONTENT_FILEPATH');
+  const json = fs.readFileSync(file);
+
+  core.setOutput("json", JSON.parse(json));
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
