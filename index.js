6dba30379cc3b95d50f2d0ac3b6c1a1b4dbefd1c
Katy DeCorah <katydecorah@gmail.com>
2019-10-08 20:15:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { writeFile } = require('fs').promises
+const { writeFile } = require('fs').promises;
 const fs = require('fs');
-const { join } = require('path')
+const { join } = require('path');
 
-async function main(score) {
-  const path = join(process.cwd(), 'scores.md')
-  await writeFile(path, `## Scores\n\n${score}`)
+async function main(md) {
+  const path = join(process.cwd(), 'scores.md');
+  await writeFile(path, md);
 }
 
 function evalScore(score) {
-  return `${score < .75 ? '🚨 ' : ''} ${score * 100.00}%`;
+  return `${score < 0.75 ? '🚨 ' : ''} ${score * 100.0}%`;
 }
 
 try {
-  const file = core.getInput('CONTENT_FILEPATH');
-  const json = JSON.parse(fs.readFileSync(file));
+  const filePath = core.getInput('FILEPATH');
+  // get all json files
+  const files = [];
+  fs.readdir(filePath, (err, files) =>
+    files.filter(file => file.extname('index.html') === '.json')
+  );
 
-  const OUTPUT_FOLDER="report";
-  const OUTPUT_PATH=`${process.env.GITHUB_WORKSPACE}/${OUTPUT_FOLDER}/scores.json`;
-  const scores = json.categories;
-
-  const score = `Category | Score
+  const md = files.reduce((str, file) => {
+    const json = JSON.parse(fs.readFileSync(file));
+    const scores = json.categories;
+    const url = json.finalUrl;
+    str += `## ${url}\n\nCategory | Score
 ---|---
 Performance | ${evalScore(scores.performance.score)}
 Accessibility | ${evalScore(scores.accessibility.score)}
 Best practices | ${evalScore(scores['best-practices'].score)}
-SEO | ${evalScore(scores.seo.score)}`
+SEO | ${evalScore(scores.seo.score)}\n\n`;
+    return str;
+  }, '## Scores\n\n');
 
-  main(score).catch(err => {
+  main(md)
+    .catch(err => {
       core.setFailed(err.message);
       process.exit(1);
-    }).then(() => {
-    console.log(`done in ${process.uptime()}s`);
-    process.exit();
-  })
-
-
+    })
+    .then(() => {
+      console.log(`done in ${process.uptime()}s`);
+      process.exit();
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
