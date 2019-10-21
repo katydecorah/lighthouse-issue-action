1822f7d7fa43ef8911d6ac100a7542822da2ac68
Katy DeCorah <katydecorah@gmail.com>
2019-10-20 20:13:08 -0400

    add tests, fix markdown fn

--- index.js
+++ index.js
@@ -11,7 +11,62 @@ async function main(md) {
 }
 
 function evalScore(score) {
-  return `${score < 0.75 ? '🚨 ' : ''} ${score * 100.0}%`;
+  return `${score < 0.75 ? '🚨' : ''}${score * 100.0}%`;
+}
+
+function markdownMe(files) {
+  const buildMd = files.reduce(
+    (obj, file) => {
+      const json = JSON.parse(fs.readFileSync(file));
+
+      const scores = json.categories;
+      const perf = scores.performance.score;
+      const accessibility = scores.accessibility.score;
+      const bestPractices = scores['best-practices'].score;
+      const seo = scores.seo.score;
+      const url = json.finalUrl;
+
+      if (
+        perf < 0.75 ||
+        accessibility < 0.75 ||
+        bestPractices < 0.75 ||
+        seo < 0.75
+      ) {
+        obj.failing.push(`### ${url}\n\nCategory | Score
+---|---
+Performance | ${evalScore(perf)}
+Accessibility | ${evalScore(accessibility)}
+Best practices | ${evalScore(bestPractices)}
+SEO | ${evalScore(seo)}`);
+      } else {
+        obj.passing.push(
+          `* ${url}\n  Performance (${evalScore(
+            perf
+          )}), Accessibility (${evalScore(
+            accessibility
+          )}), Best practices (${evalScore(bestPractices)}), SEO (${evalScore(
+            seo
+          )})`
+        );
+      }
+      return obj;
+    },
+    { failing: [], passing: [] }
+  );
+
+  return `# Lighthouse audit
+
+## Failing pages
+
+${
+    buildMd.failing.length
+      ? buildMd.failing.join('\n\n')
+      : '✨ No failing pages.'
+  }
+
+## Passing pages
+
+${buildMd.passing.length ? buildMd.passing.join('\n') : 'No passing pages.'}`;
 }
 
 try {
@@ -22,41 +77,7 @@ try {
     return arr;
   }, []);
 
-  const buildMd = files.reduce((obj, file) => {
-    const json = JSON.parse(fs.readFileSync(file));
-
-    const scores = json.categories;
-    const perf = scores.performance.score;
-    const accessibility = scores.accessibility.score;
-    const bestPractices = scores['best-practices'].score;
-    const seo = scores.seo.score;
-    const url = json.finalUrl;
-
-    if (
-      perf < 0.75 ||
-      accessibility < 0.75 ||
-      bestPractices < 0.75 ||
-      seo < 0.75
-    ) {
-      obj.failing.push(`### ${url}\n\nCategory | Score
-  ---|---
-  Performance | ${evalScore(perf)}
-  Accessibility | ${evalScore(accessibility)}
-  Best practices | ${evalScore(bestPractices)}
-  SEO | ${evalScore(seo)}`);
-} else {
-  obj.passing.push(`* ${url}: Performance (${evalScore(perf)}), Accessibility (${evalScore(accessibility)}), Best practices (${evalScore(bestPractices)}), SEO (${evalScore(seo)})`)
-}
-    return obj;
-  }, {failing: [], passing: []});
-
-  const md = `# Lighthouse audit
-## Failing pages
-${buildMd.failing.length ? buildMd.failing.join('\n\n' : '✨ No failing pages.')}
-
-## Passing pages
-${buildMd.passing.length ? buildMd.passing.join('\n') : 'No passing pages.'}
-`
+  const md = markdownMe(files);
 
   main(md)
     .catch(err => {
@@ -70,3 +91,7 @@ ${buildMd.passing.length ? buildMd.passing.join('\n') : 'No passing pages.'}
 } catch (error) {
   core.setFailed(error.message);
 }
+
+module.exports = {
+  markdownMe
+};
---
+++ test/index.test.js
@@ -0,0 +1,80 @@
+const index = require('../');
+
+test('markdownMe', () => {
+  expect(
+    index.markdownMe([
+      './test/files/1.json',
+      './test/files/2.json',
+      './test/files/3.json',
+      './test/files/4.json'
+    ])
+  ).toEqual(`# Lighthouse audit
+
+## Failing pages
+
+### https://staticmapmaker.com/one/
+
+Category | Score
+---|---
+Performance | 75%
+Accessibility | 🚨74%
+Best practices | 100%
+SEO | 🚨0%
+
+### https://staticmapmaker.com/two/
+
+Category | Score
+---|---
+Performance | 100%
+Accessibility | 100%
+Best practices | 100%
+SEO | 🚨0%
+
+## Passing pages
+
+* https://staticmapmaker.com/three/
+  Performance (100%), Accessibility (100%), Best practices (100%), SEO (100%)
+* https://staticmapmaker.com/four/
+  Performance (75%), Accessibility (75%), Best practices (75%), SEO (75%)`);
+
+  expect(index.markdownMe(['./test/files/1.json', './test/files/2.json']))
+    .toEqual(`# Lighthouse audit
+
+## Failing pages
+
+### https://staticmapmaker.com/one/
+
+Category | Score
+---|---
+Performance | 75%
+Accessibility | 🚨74%
+Best practices | 100%
+SEO | 🚨0%
+
+### https://staticmapmaker.com/two/
+
+Category | Score
+---|---
+Performance | 100%
+Accessibility | 100%
+Best practices | 100%
+SEO | 🚨0%
+
+## Passing pages
+
+No passing pages.`);
+
+  expect(index.markdownMe(['./test/files/3.json', './test/files/4.json']))
+    .toEqual(`# Lighthouse audit
+
+## Failing pages
+
+No failing pages.
+
+## Passing pages
+
+* https://staticmapmaker.com/three/
+  Performance (100%), Accessibility (100%), Best practices (100%), SEO (100%)
+* https://staticmapmaker.com/four/
+  Performance (75%), Accessibility (75%), Best practices (75%), SEO (75%)`);
+});
